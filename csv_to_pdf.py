24bf22e2feaffc999e5177452b239a8b97fa65e9
PaviNinjac <51699297+pavi-ninjaac@users.noreply.github.com>
2020-07-06 23:46:29 +0530

    changed path

--- csv_to_pdf.py
+++ csv_to_pdf.py
@@ -1,16 +1,16 @@
 # import the required libraries
 import pdfkit as pdf
 import pandas as pd
-
+# i  am considering the  current working derectory as path but youhave to set the whole path 
 #read the csv file as dataframe
-csv_file=pd.read_csv(r'F:\dash_app\dash_ibm_app\dataset\emotion.csv')
+csv_file=pd.read_csv('emotion.csv')
 
 #convert the csv to html file
-html_file=csv_file.to_html(r'F:\dash_app\dash_ibm_app\dataset\emotion.html')
+html_file=csv_file.to_html('emotion.html')
 
 #define the path for pdf file
-html_file_path=r'F:\dash_app\dash_ibm_app\dataset\emotion.html'
-pdf_file_path=r'F:\dash_app\dash_ibm_app\dataset\emotion.pdf'
+html_file_path='emotion.html'
+pdf_file_path='emotion.pdf'
 
 # convert the html to pdf
 pdf.from_file(html_file_path,pdf_file_path)
